3e1b1e9d6c4011757ec5a515039a9ab29729676e
Santiago Rendon <santiagorendon@Santiagos-MacBook-Air.local>
2019-05-03 20:57:08 -0400

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//this is added by me
+import './App.css';
+import Parser from 'rss-parser';
+let parser = new Parser();
+//this is added by me
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class Home extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      feed: [],
+    }
+    this.componentDidMount = this.componentDidMount.bind(this)
+  }
+  componentDidMount(){
+    (async () => {
+      const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+      let feed = await parser.parseURL(CORS_PROXY+'https://9gag-rss.com/api/rss/get?code=9GAGAwesome&format=2');
+      console.log(feed.title);
+      this.setState({feed: feed.items});
+      console.log(this.state.feed[0])
+    })();
+  }
+
+  render(){
+    const elements = this.state.feed.map(item => {
+      console.log("elements= ",typeof item.content);
+      return  <React.Fragment> {item.content} </React.Fragment>;
+    });
+    return (
+      <div>
+      <span dangerouslySetInnerHTML={{ __html: this.state.feed.map(player => "<p>"+player.title+"</p>"+player.content+"<br /><br /><br />").join('') }} />
+
+      </div>
+
+    )
+  }
+}
+
+ReactDOM.render(<Home/>, document.getElementById('root'));
